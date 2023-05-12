dab1fda8d3e11a5b30cf0a716703e30f86999460
Shreyo Paul <shreyopaul403@gmail.com>
2023-05-12 10:47:31 +0530

    deploy trying

--- index.js
+++ index.js
@@ -19,11 +19,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(bodyParser.json())
-app.use("/products", productsRoute)
+
 app.use(cors())
 
 app.get("", (req, res) => {
     res.send("Hello User, how are doing!?")
 })
+
+app.use("/products", productsRoute)
 
 app.listen(PORT, () => console.log("Server is running on port", PORT))
